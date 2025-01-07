c978ae261355ea54f0b394f0854c4eb392d1a460
Dominik Ferber <dominik.ferber@gmail.com>
2025-01-07 14:02:47 +0200

    reword

--- examples/snippets/app/examples/adapter-launchdarkly/page.tsx
+++ examples/snippets/app/examples/adapter-launchdarkly/page.tsx
@@ -2,5 +2,5 @@ import { summerSaleFlag } from './flags';
 
 export default async function Page() {
   const summerSale = await summerSaleFlag();
-  return <div>AdapterLaunchDarkly: {summerSale ? 'Yes' : 'No'}</div>;
+  return <div>Flag is on {summerSale ? 'Yes' : 'No'}</div>;
 }
