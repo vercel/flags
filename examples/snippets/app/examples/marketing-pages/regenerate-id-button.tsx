abeb98642fb3a2193d7c1a7b17f841b75a244c77
Dominik Ferber <dominik.ferber@gmail.com>
2025-02-01 10:30:06 +0200

    bring back darkmode for DemoFlag

--- examples/snippets/app/examples/marketing-pages/regenerate-id-button.tsx
+++ examples/snippets/app/examples/marketing-pages/regenerate-id-button.tsx
@@ -11,6 +11,7 @@ export function RegenerateIdButton() {
           'marketing-visitor-id=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT';
         window.location.reload();
       }}
+      variant="outline"
     >
       Regenerate random visitor id
     </Button>
