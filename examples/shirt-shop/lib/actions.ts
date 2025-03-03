eae57c9dd061ac96a4ad45634770a65abb598e47
Dominik Ferber <dominik.ferber@gmail.com>
2025-03-03 07:41:59 +0200

    /api/:cartId » /api/cart/:cartId

--- examples/shirt-shop/lib/actions.ts
+++ examples/shirt-shop/lib/actions.ts
@@ -5,7 +5,7 @@ import { Cart, CartItem } from '@/components/utils/cart-types';
 import { delayFlag } from '@/flags';
 import { getCartId } from './get-cart-id';
 
-const CART_BASE_URL = process.env.CART_BASE_URL;
+const BACKEND_URL = process.env.BACKEND_URL;
 
 export async function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -15,14 +15,14 @@ export async function getCart(): Promise<Cart> {
   const cartId = await getCartId();
   const delayMs = await delayFlag();
   await delay(delayMs);
-  return fetch(`${CART_BASE_URL}/api/${cartId.value}`).then((res) =>
+  return fetch(`${BACKEND_URL}/api/cart/${cartId.value}`).then((res) =>
     res.json(),
   );
 }
 
 export async function addToCart(item: CartItem) {
   const cartId = await getCartId();
-  await fetch(`${CART_BASE_URL}/api/${cartId.value}`, {
+  await fetch(`${BACKEND_URL}/api/cart/${cartId.value}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(item),
@@ -32,7 +32,7 @@ export async function addToCart(item: CartItem) {
 
 export async function removeFromCart(item: CartItem) {
   const cartId = await getCartId();
-  await fetch(`${CART_BASE_URL}/api/${cartId.value}`, {
+  await fetch(`${BACKEND_URL}/api/cart/${cartId.value}`, {
     method: 'DELETE',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(item),
