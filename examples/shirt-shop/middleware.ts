d65e23b111e8b13bd7338c1d5b2899ac39cbfecb
Dominik Ferber <dominik.ferber@gmail.com>
2025-03-03 20:40:53 +0200

    append headers

--- examples/shirt-shop/middleware.ts
+++ examples/shirt-shop/middleware.ts
@@ -31,7 +31,7 @@ export async function middleware(request: NextRequest) {
 
   // response headers
   const headers = new Headers();
-  headers.set('set-cookie', `stable-id=${stableId.value}`);
-  headers.set('set-cookie', `cart-id=${cartId.value}`);
+  headers.append('set-cookie', `stable-id=${stableId.value}`);
+  headers.append('set-cookie', `cart-id=${cartId.value}`);
   return NextResponse.rewrite(nextUrl, { request, headers });
 }
