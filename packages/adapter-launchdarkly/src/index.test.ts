f7fb9ad0dbe8359c622931b455ffa11ff21b2f59
Dominik Ferber <dominik.ferber@gmail.com>
2025-03-08 13:22:54 +0200

    change to type import

--- packages/adapter-launchdarkly/src/index.test.ts
+++ packages/adapter-launchdarkly/src/index.test.ts
@@ -1,6 +1,6 @@
 import type { ReadonlyHeaders, ReadonlyRequestCookies } from 'flags';
 import { expect, it, describe, vi, beforeAll } from 'vitest';
-import { ldAdapter, LDContext } from '..';
+import { ldAdapter, type LDContext } from '..';
 
 const ldClientMock = {
   waitForInitialization: vi.fn(),
