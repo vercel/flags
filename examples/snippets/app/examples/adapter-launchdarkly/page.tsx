ff51af2abdbf647ef88b527775fff875e256c3f3
Dominik Ferber <dominik.ferber@gmail.com>
2025-03-08 08:27:18 +0200

    remove adapter-launchdarkly snippet

--- examples/snippets/app/examples/adapter-launchdarkly/page.tsx
+++
@@ -1,6 +0,0 @@
-import { summerSaleFlag } from './flags';
-
-export default async function Page() {
-  const summerSale = await summerSaleFlag();
-  return <div>Flag is on {summerSale ? 'Yes' : 'No'}</div>;
-}
