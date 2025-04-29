2aac8ec02a10997cd6f86a647a0e0d3876911381
Tiago Fonseca <tiago@tiagopaiva.me>
2025-04-29 18:03:54 -0300

    address changes

--- packages/adapter-flagsmith/src/index.ts
+++ packages/adapter-flagsmith/src/index.ts
@@ -1,23 +1,40 @@
 import type { Adapter } from 'flags';
-import flagsmith, { IFlagsmithFeature, IInitConfig } from 'flagsmith';
+import flagsmith, {
+  IFlagsmithFeature,
+  IInitConfig,
+  IIdentity,
+  createFlagsmithInstance,
+} from 'flagsmith';
 
 type AdapterParams = {
   key?: string;
+  projectID?: string;
+  dashboardURL?: string;
 } & IInitConfig;
 
 export function createFlagsmithAdapter<
   ValueType extends IFlagsmithFeature,
-  EntitiesType,
+  EntitiesType extends IIdentity,
 >({
   key: customKey,
+  environmentID,
+  projectID,
+  dashboardURL,
   ...configParams
 }: AdapterParams): Adapter<ValueType, EntitiesType> {
-  async function initialize(config: IInitConfig) {
-    if (flagsmith?.initialised) return;
-    await flagsmith.init({ fetch: global.fetch, ...configParams });
+  async function initialize() {
+    const { identity, ...restConfigParams } = configParams;
+    const flagsmithInstance = createFlagsmithInstance();
+    await flagsmithInstance.init({ fetch: global.fetch, ...restConfigParams });
+    return flagsmithInstance;
   }
 
   return {
+    origin: (key: string) => {
+      const state = flagsmith.getState();
+      const flag = state?.flags?.[key];
+      return `${dashboardURL}/project/${projectID}/environment/${environmentID}/features/?feature=${flag?.id}`;
+    },
     async decide({
       key,
       entities,
@@ -25,8 +42,17 @@ export function createFlagsmithAdapter<
       cookies,
       defaultValue,
     }): Promise<ValueType> {
-      await initialize(configParams);
-      const state = flagsmith.getState();
+      const identity = entities;
+      const flagsmithInstance = await initialize();
+      const state = flagsmithInstance.getState();
+
+      flagsmithInstance.setState({
+        ...state,
+        evaluationContext: {
+          identity,
+        },
+      });
+
       const keyName = customKey || key;
       const flag = state?.flags?.[keyName] ?? defaultValue;
       return flag as ValueType;
@@ -38,14 +64,18 @@ export function createFlagsmithAdapter<
 export const flagsmithAdapter = {
   getFeature: <ValueType extends IFlagsmithFeature>(params?: AdapterParams) => {
     const environmentID = process.env.FLAGSMITH_ENVIRONMENT_ID;
+    const dashboardURL = process.env.FLAGSMITH_DASHBOARD_URL;
+    const projectID = process.env.FLAGSMITH_PROJECT_ID;
     if (!environmentID && params?.environmentID) {
       throw new Error(
         '@flags-sdk/flagsmith: FLAGSMITH_ENVIRONMENT_ID is not set',
       );
     }
 
-    const adapter = createFlagsmithAdapter<ValueType, unknown>({
+    const adapter = createFlagsmithAdapter<ValueType, IIdentity>({
       environmentID,
+      dashboardURL,
+      projectID,
       ...params,
     });
 
