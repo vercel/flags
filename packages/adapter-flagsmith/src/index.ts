3a4439be6df8e354ca1b7921f98f0007c094e193
Tiago Fonseca <tiago@tiagopaiva.me>
2025-05-06 18:13:07 -0300

    implements provider

--- packages/adapter-flagsmith/src/index.ts
+++ packages/adapter-flagsmith/src/index.ts
@@ -1,84 +1,70 @@
 import type { Adapter } from 'flags';
-import flagsmith, {
-  IFlagsmithFeature,
-  IInitConfig,
-  IIdentity,
-  createFlagsmithInstance,
-} from 'flagsmith';
+import flagsmith from 'flagsmith';
+import { IIdentity, IInitConfig, IFlagsmithFeature } from 'flagsmith/types';
 
-type AdapterParams = {
-  key?: string;
-  projectID?: string;
-  dashboardURL?: string;
-} & IInitConfig;
+export { getProviderData } from './provider';
+
+let defaultFlagsmithAdapter:
+  | ReturnType<typeof createFlagsmithAdapter>
+  | undefined;
+
+export type FlagsmithValue = IFlagsmithFeature['value'];
 
 export function createFlagsmithAdapter<
-  ValueType extends IFlagsmithFeature,
+  ValueType extends FlagsmithValue,
   EntitiesType extends IIdentity,
->({
-  key: customKey,
-  environmentID,
-  projectID,
-  dashboardURL,
-  ...configParams
-}: AdapterParams): Adapter<ValueType, EntitiesType> {
-  async function initialize() {
-    const { identity, ...restConfigParams } = configParams;
-    const flagsmithInstance = createFlagsmithInstance();
-    await flagsmithInstance.init({ fetch: global.fetch, ...restConfigParams });
-    return flagsmithInstance;
-  }
-
+>(params: IInitConfig): Adapter<ValueType, EntitiesType> {
   return {
-    origin: (key: string) => {
-      const state = flagsmith.getState();
-      const flag = state?.flags?.[key];
-      return `${dashboardURL}/project/${projectID}/environment/${environmentID}/features/?feature=${flag?.id}`;
-    },
     async decide({
       key,
-      entities,
-      headers,
-      cookies,
       defaultValue,
+      entities,
+    }: {
+      key: string;
+      defaultValue?: ValueType;
+      entities?: EntitiesType;
+      headers: any;
+      cookies: any;
     }): Promise<ValueType> {
-      const identity = entities;
-      const flagsmithInstance = await initialize();
-      const state = flagsmithInstance.getState();
+      await flagsmith.init({ fetch: globalThis.fetch, ...params });
+      const identity = entities?.[0];
 
-      flagsmithInstance.setState({
-        ...state,
-        evaluationContext: {
-          identity,
-        },
-      });
+      if (identity) {
+        await flagsmith.identify(identity);
+      }
+      const state = flagsmith.getState();
+      const flagState = state.flags?.[key];
 
-      const keyName = customKey || key;
-      const flag = state?.flags?.[keyName] ?? defaultValue;
-      return flag as ValueType;
+      if (!flagState || !flagState?.enabled) {
+        return defaultValue as ValueType;
+      }
+
+      return flagState.value as ValueType;
     },
   };
 }
 
+function assertEnv(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Flagsmith Adapter: Missing ${name} environment variable`);
+  }
+  return value;
+}
+
+export const getOrCreateDefaultFlagsmithAdapter = () => {
+  if (!defaultFlagsmithAdapter) {
+    const environmentID = assertEnv('FLAGSMITH_ENVIRONMENT_ID');
+    defaultFlagsmithAdapter = createFlagsmithAdapter({
+      environmentID,
+    });
+  }
+  return defaultFlagsmithAdapter;
+};
+
 // Lazy default adapter
 export const flagsmithAdapter = {
-  getFeature: <ValueType extends IFlagsmithFeature>(params?: AdapterParams) => {
-    const environmentID = process.env.FLAGSMITH_ENVIRONMENT_ID;
-    const dashboardURL = process.env.FLAGSMITH_DASHBOARD_URL;
-    const projectID = process.env.FLAGSMITH_PROJECT_ID;
-    if (!environmentID && params?.environmentID) {
-      throw new Error(
-        '@flags-sdk/flagsmith: FLAGSMITH_ENVIRONMENT_ID is not set',
-      );
-    }
-
-    const adapter = createFlagsmithAdapter<ValueType, IIdentity>({
-      environmentID,
-      dashboardURL,
-      projectID,
-      ...params,
-    });
-
-    return adapter;
+  getFeatureValue() {
+    return getOrCreateDefaultFlagsmithAdapter();
   },
 };
