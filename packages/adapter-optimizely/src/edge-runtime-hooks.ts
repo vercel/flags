71c1384021ee2209f884168f510775c3c2a3e145
Boris Besemer <borisbesemer@gmail.com>
2025-09-19 16:13:42 +0200

    chore: clean up types and old decide type signature

--- packages/adapter-optimizely/src/edge-runtime-hooks.ts
+++ packages/adapter-optimizely/src/edge-runtime-hooks.ts
@@ -1,8 +1,10 @@
-import { LogEvent } from '@optimizely/optimizely-sdk';
+import type { LogEvent } from '@optimizely/optimizely-sdk';
 
 /**
  * Web standards friendly event dispatcher for Optimizely
- * uses `after` to avoid blocking the visitor's page load
+ * uses `waitUntil()` to avoid blocking the visitor's page load
+ *
+ * This does not send back the status code to the dispatcher as it runs in `waitUntil()`
  */
 export async function dispatchEvent(event: LogEvent) {
   // Non-POST requests not supported
@@ -21,21 +23,9 @@ export async function dispatchEvent(event: LogEvent) {
     headers: {
       'Content-Type': 'application/json',
     },
-  })
-    .then((response) => {
-      if (response.ok) {
-        return { statusCode: response.status };
-      }
-    })
-    .catch((error) => {
-      console.error('Error dispatching event:', error);
-    });
+  });
 
-  try {
-    import('@vercel/functions').then(({ waitUntil }) => {
-      waitUntil(dispatch);
-    });
-  } catch (error) {
-    console.error('Error dispatching event:', error);
-  }
+  import('@vercel/functions').then(({ waitUntil }) => {
+    waitUntil(dispatch);
+  });
 }
