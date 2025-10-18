7f602ce9a4ad614983c76d55d34d1480d07b336f
Dominik Ferber <dominik.ferber@gmail.com>
2025-10-18 21:30:13 +0300

    expose OpenFeature adapter

--- packages/vercel-flags-core/tsup.config.js
+++ packages/vercel-flags-core/tsup.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/openfeature.ts'],
   format: 'esm',
   splitting: true,
   sourcemap: true,
