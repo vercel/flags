007aea0946f581287e02a6b30e5f6374ed3fd686
Dominik Ferber <dominik.ferber@gmail.com>
2025-10-24 10:07:35 +0300

    update comment

--- packages/vercel-flags-core/src/store.ts
+++ packages/vercel-flags-core/src/store.ts
@@ -1,7 +1,7 @@
 import { AsyncLocalStorage } from 'node:async_hooks';
 
 /**
- * A temporary store to avoid reading the Edge Config for every flag,
+ * A store to avoid reading the Edge Config for every flag evaluation,
  * and instead reading it once per request.
  */
 export const store = new AsyncLocalStorage<WeakKey>();
