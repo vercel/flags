4efc0136bd6a24b59ae0cf3a3d01c80a661c4dde
Dominik Ferber <dominik.ferber@gmail.com>
2025-12-07 07:48:25 +0200

    step

--- packages/vercel-flags-core/src/data-source/flag-network-data-source.ts
+++ packages/vercel-flags-core/src/data-source/flag-network-data-source.ts
@@ -1,5 +1,5 @@
-import { store } from '../store';
-import type { DataSourceData, Packed } from '../types';
+import type { BundledDefinitions } from '../types';
+import { readBundledDefinitions } from '../utils/read-bundled-definitions';
 import type { DataSource } from './interface';
 
 /**
@@ -7,39 +7,21 @@ import type { DataSource } from './interface';
  */
 export class FlagNetworkDataSource implements DataSource {
   sdkKey?: string;
-  requestCache: WeakMap<WeakKey, Promise<Packed.Data | undefined>>;
+  bundledDefinitions: BundledDefinitions | null = null;
 
   constructor(options: {
     sdkKey: string;
   }) {
     this.sdkKey = options.sdkKey;
-    this.requestCache = new WeakMap();
 
     // preload from embedded json AND set up stream,
     // and only ever read from in-memory data
-  }
-
-  // This is a temporary solution to avoid reading the Edge Config for every flag,
-  // and instead reading it once per request.
-  private async getCachedData(): Promise<DataSourceData | undefined> {
-    throw new Error('not implemented yet');
-    // const cacheKey = store.getStore();
-    // if (cacheKey) {
-    //   const cached = this.requestCache.get(cacheKey);
-    //   if (cached) {
-    //     return cached;
-    //   }
-    // }
-    // const promise = this.edgeConfigClient.get<Packed.Data>(
-    //   this.edgeConfigItemKey,
-    // );
-    // if (cacheKey) this.requestCache.set(cacheKey, promise);
-    // return promise;
+    this.bundledDefinitions = readBundledDefinitions(this.sdkKey);
   }
 
   // called once per flag rather than once per request
   async getData() {
-    const data = await this.getCachedData();
+    const data = this.bundledDefinitions;
     if (!data) throw new Error(`No definitions found`);
     return data;
   }
