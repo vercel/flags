e7e2b78f9d0559f4cc9519b735dbd9153b84fbe7
Luis Meyer <luis.meyer@vercel.com>
2026-01-15 15:16:33 +0100

    add shirt-shop-vercel

---
+++ examples/shirt-shop-vercel/components/utils/images.ts
@@ -0,0 +1,11 @@
+import black from '@/public/images/product/shirt-black.png';
+import blue from '@/public/images/product/shirt-blue.png';
+import white from '@/public/images/product/shirt-white.png';
+
+export const images = [black, white, blue];
+
+export const colorToImage: Record<string, any> = {
+  Black: black,
+  White: white,
+  Blue: blue,
+};
