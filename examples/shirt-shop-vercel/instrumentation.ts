f6371503d9297f1c66ef0b1fb386531beb1c2a81
Dominik Ferber <dominik.ferber@gmail.com>
2026-01-25 17:08:48 +0200

    require fallbacks at build time on vercel only

--- examples/shirt-shop-vercel/instrumentation.ts
+++ examples/shirt-shop-vercel/instrumentation.ts
@@ -1,5 +1,7 @@
 import { flagsClient } from '@vercel/flags-core';
 
 export async function register() {
-  await flagsClient.ensureFallback();
+  if (process.env.CI === '1') {
+    await flagsClient.ensureFallback();
+  }
 }
