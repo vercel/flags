0569fdb3780f920ac627d10cece3e8fb72b6f3ed
Dominik Ferber <dominik.ferber@gmail.com>
2026-01-31 08:41:34 +0200

    use new format

--- packages/vercel-flags-core/src/utils/read-bundled-definitions.ts
+++ packages/vercel-flags-core/src/utils/read-bundled-definitions.ts
@@ -1,31 +1,25 @@
-// The definitions.json file is created at build time by the customer's app
-// in node_modules/@vercel/flags-definitions/. This is a fallback
-// mechanism used so the app can always fall back to a bundled version of
-// the definitions, even if the flags network is degraded or unavailable.
+// The @vercel/flags-definitions module is created at build time by Vercel CLI
+// in node_modules/. This is a fallback mechanism used so the app can always
+// fall back to a bundled version of the definitions, even if the flags network
+// is degraded or unavailable.
 //
-// At build time of the customer's app, the definitions.json file is created
-// using the "vercel-flags prepare" script, which also creates a package.json
-// that exports definitions.json.
-//
-// If the "vercel-flags prepare" script did not run, the import will fail
-// and we return null.
+
 import type { BundledDefinitions, BundledDefinitionsResult } from '../types';
 
-type DefinitionsJson = Record<string, BundledDefinitions>;
-
 /**
- * Reads the local definitions that get bundled at build time (definitions.json).
+ * Reads the local definitions that get bundled at build time.
  */
 export async function readBundledDefinitions(
-  id: string,
+  sdkKey: string,
 ): Promise<BundledDefinitionsResult> {
-  let stores: DefinitionsJson;
+  let get: (sdkKey: string) => BundledDefinitions | null;
   try {
-    stores = await import(
-      /* webpackIgnore: true */
+    const module = await import(
+      /* turbopackOptional: true */
       // @ts-expect-error this only exists at build time
-      '@vercel/flags-definitions/definitions.json'
+      '@vercel/flags-definitions'
     );
+    get = module.get;
   } catch (error) {
     // If the module doesn't exist, the prepare script didn't run
     if (
@@ -58,7 +52,7 @@ export async function readBundledDefinitions(
     return { definitions: null, state: 'unexpected-error', error };
   }
 
-  const entry = stores && Object.hasOwn(stores, id) ? stores[id] : null;
+  const entry = get(sdkKey);
   if (!entry) return { definitions: null, state: 'missing-entry' };
   return { definitions: entry, state: 'ok' };
 }
