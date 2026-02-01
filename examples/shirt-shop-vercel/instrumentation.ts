79397c08a6c21cf2e952124c1a14c05f60cccd00
Dominik Ferber <dominik.ferber@gmail.com>
2026-02-01 17:54:54 +0200

    update usage

--- examples/shirt-shop-vercel/instrumentation.ts
+++ examples/shirt-shop-vercel/instrumentation.ts
@@ -2,6 +2,7 @@ import { flagsClient } from '@vercel/flags-core';
 
 export async function register() {
   if (process.env.CI === '1') {
-    await flagsClient.ensureFallback();
+    const fallback = await flagsClient.getFallbackDatafile();
+    if (!fallback) throw new Error('Missing fallback datafile');
   }
 }
