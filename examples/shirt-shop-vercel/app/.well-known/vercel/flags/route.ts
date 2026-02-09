be8832830ec39ddf572d50494fd1b790b2460314
Dominik Ferber <dominik.ferber@gmail.com>
2026-02-09 09:39:48 +0200

    rm example

--- examples/shirt-shop-vercel/app/.well-known/vercel/flags/route.ts
+++
@@ -1,8 +0,0 @@
-import { getProviderData } from '@flags-sdk/vercel';
-import { createFlagsDiscoveryEndpoint } from 'flags/next';
-import * as flags from '../../../../flags';
-
-export const GET = createFlagsDiscoveryEndpoint(async (request) => {
-  const providerData = await getProviderData(flags);
-  return providerData;
-});
--- examples/shirt-shop-vercel/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-import { flagsClient } from '@vercel/flags-core';
-
-export async function register() {
-  if (process.env.CI === '1') {
-    const fallback = await flagsClient.getFallbackDatafile();
-    if (!fallback) throw new Error('Missing fallback datafile');
-  }
-}
