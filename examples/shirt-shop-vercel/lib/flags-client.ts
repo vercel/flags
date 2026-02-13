c45437c8721cdce2f8dfbf572a6c0b2922a52f4c
Dominik Ferber <dominik.ferber@gmail.com>
2026-02-13 13:34:45 +0200

    rm shirt-shop-vercel

--- examples/shirt-shop-vercel/lib/flags-client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@vercel/flags-core';
-import { wrapFetch } from './debug-fetch';
-
-export const flagsClient = createClient(process.env.FLAGS as string, {
-  fetch: wrapFetch(fetch.bind(globalThis)),
-});
