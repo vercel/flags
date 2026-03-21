9142790975004bd250374ff504aa015e6203fe25
Dominik Ferber <dominik.ferber@gmail.com>
2026-03-21 21:17:04 +0200

    only build publishable packages during release process (#348)
    * clean up
    
    * fix release process

--- packages/vercel-flags-core/src/types/flags-definitions.d.ts
+++ packages/vercel-flags-core/src/types/flags-definitions.d.ts
@@ -1,8 +1,3 @@
-declare module '@vercel/flags-definitions/definitions.json' {
-  const definitions: unknown;
-  export default definitions;
-}
-
 declare module '@vercel/flags-definitions' {
   export function get(hashedSdkKey: string): Record<string, unknown> | null;
   export const version: string;
