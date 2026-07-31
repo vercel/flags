01888f8aaae1ececc9ed16030c6cc98f1b9977b6
Dominik Ferber <dominik.ferber@gmail.com>
2026-07-31 15:50:10 -0400

    try

--- examples/shirt-shop/pages/api/discovery.ts
+++ examples/shirt-shop/pages/api/discovery.ts
@@ -2,5 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('x-flags-sdk-version', '4.2.4');
+  res.setHeader('cache-control', 'no-store');
+  res.setHeader('content-type', 'application/json');
   res.status(200).json({});
 }
